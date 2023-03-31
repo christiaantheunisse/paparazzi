0377b91aa9cd2cef7c35c61f91cf8e838dc9da61
Christiaan Theunisse <ctheunisse@tudelft.nl>
2023-03-31 14:15:36 +0200

    Started documentation on wrong branch

--- sw/airborne/modules/of_avoidance/object_avoider.c
+++ sw/airborne/modules/of_avoidance/object_avoider.c
@@ -1,23 +1,3 @@
-/*
- * Copyright (C) Roland Meertens
- *
- * This file is part of paparazzi
- *
- */
-/**
- * @file "modules/orange_avoider/orange_avoider.c"
- * @author Roland Meertens
- * Example on how to use the colours detected to avoid orange pole in the cyberzoo
- * This module is an example module for the course AE4317 Autonomous Flight of Micro Air Vehicles at the TU Delft.
- * This module is used in combination with a color filter (cv_detect_color_object) and the navigation mode of the autopilot.
- * The avoidance strategy is to simply count the total number of orange pixels. When above a certain percentage threshold,
- * (given by color_count_frac) we assume that there is an obstacle and we turn.
- *
- * The color filter settings are set using the cv_detect_color_object. This module can run multiple filters simultaneously
- * so you have to define which filter to use with the ORANGE_AVOIDER_VISUAL_DETECTION_ID setting.
- */
-
-//#include "modules/orange_avoider/orange_avoider.h"
 #include "firmwares/rotorcraft/navigation.h"
 #include "generated/airframe.h"
 #include "state.h"
@@ -30,8 +10,6 @@
 #include "generated/flight_plan.h"
 
 #define OBJECT_AVOIDER_VERBOSE TRUE
-
-
 
 #define PRINT(string,...) fprintf(stderr, "[object_avoider->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
 #if OBJECT_AVOIDER_VERBOSE
@@ -49,18 +27,16 @@ static void lowest_index_cb(uint8_t sender_id, uint8_t i_safe);
 enum navigation_state_t {
   SAFE,
   OBSTACLE_FOUND,
-  SEARCH_FOR_SAFE_HEADING,
   OUT_OF_BOUNDS
 };
 
 
 // define and initialise global variables
-enum navigation_state_t navigation_state = SEARCH_FOR_SAFE_HEADING;
+enum navigation_state_t navigation_state = SAFE;
 int32_t obstacle_dist = 0;                // Distance to closest object
 int16_t obstacle_free_confidence = 0;   // a measure of how certain we are that the way ahead is safe.
 float angle = 0;
 float heading_increment = 100.f;          // heading angle increment [deg]
-float maxDistance = 2.25;               // max waypoint displacement [m]
 int16_t centerTheshold = 2;
 int16_t largeLeft = 0;
 int16_t smallLeft = 1;
@@ -90,33 +66,19 @@ int param_N_DIRBLOCKS = N_DIRBLOCKS;
 int direction_accumulator[N_DIRBLOCKS] = {0};
 int center_index = N_DIRBLOCKS / 2;
 
-const int16_t max_trajectory_confidence = 5; // number of consecutive negative object detections to be sure we are obstacle free
-
-// Pause vars
+// Variables used to pause the confidence updates during turning
 bool do_pause = false;
 struct timeval start_time, now;
-//clock_t start_time, now;
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
- * This next section defines an ABI messaging event (http://wiki.paparazziuav.org/wiki/ABI), necessary
- * any time data calculated in another module needs to be accessed. Including the file where this external
- * data is defined is not enough, since modules are executed parallel to each other, at different frequencies,
- * in different threads. The ABI event is triggered every time new data is sent out, and as such the function
- * defined in this file does not need to be explicitly called, only bound in the init function
- */
 
 static abi_event lowestFilteredIndex;
-// Global var with lowest index
-uint8_t lowest_index = 120;
+uint8_t lowest_index = 120; // Just a arbitrary value -> Is set in the range [0, N_DIRBLOCKS - 1]
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// ABI DIVERGENCE_SAFE_HEADING callback
-// Callback should always have `uint8_t sender_id`
+// Callback for the ABI DIVERGENCE_SAFE_HEADING
+// These messages are send by the image processing thread. It contains the index of the direction with the lowest
+// amount of obstacles detected.
 static void lowest_index_cb(uint8_t sender_id, uint8_t i_safe) {
-//    printf("ABI receives new index: %d", i_safe);
-    printf("Pause: %d", do_pause);
+    // If the drone is not turning, so it shouldn't pause
+    // +1 to the preferred index and -1 for all other indeces
     if (!do_pause) {
         for (int i = 0; i < N_DIRBLOCKS; i++) {
             if (i == i_safe) {
@@ -132,15 +94,15 @@ static void lowest_index_cb(uint8_t sender_id, uint8_t i_safe) {
 
             }
         }
+    // If the drone is turning and should pause:
+    // - Don't update the confidence
+    // - If the pausing is over; reset the confidence values
     } else {
         gettimeofday(&now, NULL);
-//        now = clock();
         unsigned long difference = (now.tv_sec - start_time.tv_sec) * 1000000 + now.tv_usec - start_time.tv_usec;
-        printf("Difference: %lu us", difference);
 
+        // Check if the pausing should stop
         if ((difference) >= (pause_time * 100 * 1000)) { // * 100 ms * 1000 (us -> ms)
-//        if ((double)(now - start_time)/CLOCKS_PER_SEC >= PAUSE_TIME) {
-            printf("STOP PAUSING ==========================");
             do_pause = false;
             for (int i = 0; i < N_DIRBLOCKS; i++) {
                 direction_accumulator[i] = 0;
@@ -149,24 +111,17 @@ static void lowest_index_cb(uint8_t sender_id, uint8_t i_safe) {
     }
 }
 
-
-/*
- * Initialisation function, setting the colour filter, random seed and heading_increment
- */
+// Init function where the ABI is binded
 void object_avoider_init(void)
 {
   // Initialise random values
   srand(time(NULL));
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  // ABI_BROADCAST = 255 = receive messages from all senders on DIVERGENCE_SAFE_HEADING
+  // Simply subscribe to all the sent ABI messages of type DIVERGENCE_SAFE_HEADING
   AbiBindMsgDIVERGENCE_SAFE_HEADING(ABI_BROADCAST, &lowestFilteredIndex, lowest_index_cb);
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 
-/*
- * Function that checks it is safe to move forwards, and then moves a waypoint forward or changes the heading
- */
+// The autopilot function that decides whether to turn or not
 void object_avoider_periodic(void)
 {
     // only evaluate our state machine if we are flying
@@ -174,8 +129,7 @@ void object_avoider_periodic(void)
         return;
     }
 
-//    float moveDistance = fminf(maxDistance, 0.2f * obstacle_free_confidence);
-    float moveDistance = fminf(maxDistance, 0.5f);
+    float moveDistance = 0.5;
     int angle;
 
     switch (navigation_state){
@@ -202,14 +156,6 @@ void object_avoider_periodic(void)
             // navigation_state = SEARCH_FOR_SAFE_HEADING;
             navigation_state = SAFE;
 
-            break;
-        case SEARCH_FOR_SAFE_HEADING: // Currently not used
-            increase_nav_heading(heading_increment);
-
-            // make sure we have a couple of good readings before declaring the way safe
-            if (obstacle_free_confidence >= 2){
-                navigation_state = SAFE;
-            }
             break;
         case OUT_OF_BOUNDS:
             increase_nav_heading(heading_increment);
